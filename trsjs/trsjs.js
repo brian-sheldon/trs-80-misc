48d0c2a4ed5bdb18b2d1388d7d3cdfb0463995fa
Brian Sheldon <brian.sheldon@live.ca>
2026-04-20 21:29:59 -0700

    added xterm

--- trsjs/trsjs.js
+++ trsjs/trsjs.js
@@ -8,11 +8,17 @@ function start() {
 
   log( 'starting ...' );
 
+  log( 'TRS-80 Emulator Loading ...' );
+
   trsEmu.screen('scrn').button('btn').perf('graph').focus();
+  
+  log( 'xterm loading ...' );
 
   let term = new Terminal();
   term.open( document.getElementById( 'console' ) );
   term.write( 'Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ' );
+  
+  log( 'end of start ...' );
 
 }
 
